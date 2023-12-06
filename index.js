1924a403d8156e2ed11121d27e47d65f7d9c9480
jjsanjim <138830212+jjsanjim@users.noreply.github.com>
2023-12-06 16:35:57 -0600

    comments of how to upload

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log("adios equipo");
 const Ale =true;
+
+/*  1.- guardar cambios (hacer un commit ) en la rama que estan trabajando
+    2.-cambiar a la rama develop
+    3.- cuando esten en la rama develop hacer un git pull origin develop
+    4.- vuelvan a la rama donde trabajaban
+    5.-  hacer el comando git rebase develop
+
+    git push -f origin nombreRama */
